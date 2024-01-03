652a3f5c843315501b224ad18466f84a7e740e47
l-guerra-rago <lylian.gr@protonmail.com>
2024-01-03 17:12:50 +0100

    WIP

--- assets/javascript/filmlist.js
+++ assets/javascript/filmlist.js
@@ -1,5 +1,17 @@
 let filmListArray = document.querySelectorAll(".film-list");
 
+function animationScroll(element, number) {
+    let scrollLeft = element.scrollLeft;
+    let offsetWidth = element.offsetWidth;
+    let scroll = scrollLeft + offsetWidth * number;
+
+    element.scrollTo({
+        top: 0,
+        left: scroll,
+        behavior: "smooth"
+    });
+}
+
 filmListArray.forEach((filmList) => {
     let filmListAction = filmList.querySelectorAll(".film-list-action");
     let filmListList = filmList.querySelector(".film-list-list");
@@ -7,11 +19,11 @@ filmListArray.forEach((filmList) => {
     filmListAction.forEach((action) => {
         if (action.classList.contains("film-list-action-left")) {
             action.addEventListener("click", () => {
-                filmListList.scrollLeft -= filmListList.offsetWidth;
+                animationScroll(filmListList, -1)
             });
         } else {
             action.addEventListener("click", () => {
-                filmListList.scrollLeft += filmListList.offsetWidth;
+                animationScroll(filmListList, 1)
             });
         }
     });
